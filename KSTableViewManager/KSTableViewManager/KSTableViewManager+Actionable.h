97c9a0e4d5cfecf9480e95a3786c9b784a9f288e
Konrad Szczesniak <konrad.szczesniak@elpassion.pl>
2016-03-20 13:15:45 +0100

    convinent category for easy operating on single section manager

---
+++ KSTableViewManager/KSTableViewManager/KSTableViewManager+Actionable.h
@@ -0,0 +1,11 @@
+//
+// Created by Konrad Szczesniak on 20/03/16.
+// Copyright (c) 2016 Konrad Szczesniak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KSTableViewManager.h"
+#import "KSTableViewActionable.h"
+
+@interface KSTableViewManager (Actionable)<KSTableViewActionable>
+@end
